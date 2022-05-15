869ff8f5b2f6ead68551cc0503fb86bfe272a97f
marciovmf <marciovmf@gmail.com>
2022-05-16 00:59:11 +0200

    Load/parse variables.txt at launcher startup.
    Added window size and caption as config variables.

--- src/launcher/smol_launcher.cpp
+++ src/launcher/smol_launcher.cpp
@@ -8,6 +8,7 @@
 #include <smol/smol_renderer.h>
 #include <smol/smol_systems_root.h>
 #include <smol/smol_cfg_parser.h>
+#include <smol/smol_color.h>
 
 #if defined(SMOL_DEBUG)
 #define SMOL_LOGFILE nullptr
@@ -25,63 +26,44 @@
 #endif
 #endif
 
+#define SMOL_VARIABLES_FILE ((const char*) "assets/variables.txt")
+
 namespace smol
 {
   namespace launcher
   {
+    struct WindowVariables
+    {
+      Vector2 size;
+      const char* caption;
+    }; 
+
     int smolMain(int argc, char** argv)
     {
       smol::Log::verbosity(SMOL_LOGLEVEL);
       if (SMOL_LOGFILE != nullptr)
         smol::Log::toFile(SMOL_LOGFILE);
 
-      if (!Platform::initOpenGL(3, 2))
-        return 1;
-
-#define TEST_CFG_PARSING 1
-#if TEST_CFG_PARSING
-      {
-      Config config("assets/test.txt");
-      debugLogInfo("Num Entries = %d", config.entryCount);
-
+      // parse variables file
+      WindowVariables windowVariables;
+      Config config(SMOL_VARIABLES_FILE);
       ConfigEntry* entry = config.entries;
+
       for (int i = 0; i < config.entryCount; i++)
       {
-        debugLogInfo("Entry #%d: %d variables.", i, entry->variableCount);
-
-        for (int varIndex = 0; varIndex < entry->variableCount; varIndex++)
+        if (strncmp(entry->variables[0].name ,"window", strlen("window")) == 0)
         {
-          ConfigVariable* variable = &entry->variables[varIndex];
-
-          if (variable->type == ConfigVariable::NUMBER)
-            debugLogInfo("\t%s = %f", variable->name, variable->numberValue);
-          else if (variable->type == ConfigVariable::STRING)
-            debugLogInfo("\t%s = %s", variable->name, variable->stringValue);
-          else if (variable->type == ConfigVariable::VECTOR2)
-            debugLogInfo("\t%s = %f, %f", variable->name,
-                variable->vec2Value[0],
-                variable->vec2Value[1]);
-          else if (variable->type == ConfigVariable::VECTOR3)
-            debugLogInfo("\t%s = %f, %f, %f", variable->name,
-                variable->vec3Value[0],
-                variable->vec3Value[1],
-                variable->vec3Value[2]);
-          else if (variable->type == ConfigVariable::VECTOR4)
-            debugLogInfo("\t%s = %f, %f, %f, %f", variable->name,
-                variable->vec4Value[0],
-                variable->vec4Value[1],
-                variable->vec4Value[2],
-                variable->vec4Value[3]);
+          // Window variables
+          windowVariables.size = entry->getVariableVec2("size");
+          windowVariables.caption = entry->getVariableString("caption");
         }
-
 
         entry = entry->next;
       }
 
-      debugLogInfo("done");
 
-      }
-#endif
+      if (!Platform::initOpenGL(3, 2))
+        return 1;
 
       smol::Module* game = Platform::loadModule(SMOL_GAME_MODULE_NAME);
       SMOL_GAME_CALLBACK_ONSTART onGameStartCallback = (SMOL_GAME_CALLBACK_ONSTART)
@@ -99,9 +81,8 @@ namespace smol
         return 1;
       }
 
-      const int WIDTH = 1080;
-      const int HEIGHT = 768;
-      smol::Window* window = Platform::createWindow(WIDTH, HEIGHT, (const char*)"Smol Engine");
+      smol::Window* window = Platform::createWindow((int) windowVariables.size.x,
+          (int) windowVariables.size.y, windowVariables.caption);
 
       // Initialize systems root
       smol::SystemsRoot root;
